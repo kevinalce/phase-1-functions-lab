a60e6b0762067427113ed8edbf463288f19c2d39
Kevin Alce <Kevinalce33@gmail.com>
2022-06-16 20:59:21 -0400

    commit tree a6d0b6a1ea2b58a24661271741a6880a6687976e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const hq = 42;
+const feet = 264;
+function distanceFromHqInBlocks(pickUp){
+    if (pickup > hq){
+        return pickUp - hq;
+    }else if(pickUp < hq);
+    return hq - pickUp
+}
